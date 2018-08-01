9bbad93a66a39fd0b92760cdba6708b9407e6c7a
Elijah Harmon <elijahharmon@gmail.com>
2018-08-01 14:36:37 -0400

    Adding basics for mission 1

---
+++ python/Missions/mission_1/bottles.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Author: ENTER_NAME
+Description: ENTER_DESC
+"""
+
+def main():
+    """
+    Write your code inside the main function
+    """
+
+if __name__ == '__main__':
+    main()
