97435285910656ccd6fcff9e6285fe49984ffe79
Elijah Harmon <elijahharmon@gmail.com>
2018-08-01 15:15:11 -0400

    Added mission 2 breif

--- python/Missions/mission_2/starter_code.py
+++ python/Missions/mission_2/starter_code.py
@@ -3,15 +3,12 @@
 Author: ENTER_NAME
 Description: ENTER_DESC
 """
-import sys
 
 # Write your code in this function
 def main():
     """
     Write your code inside the main function
     """
-    print(len(sys.argv))
-    print(sys.argv)
 
 # Don't worry if you don't know what this does
 if __name__ == '__main__':
