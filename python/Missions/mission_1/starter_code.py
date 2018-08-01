1f22a066a44473d837dfd0592eb82308ab6b71e2
Elijah Harmon <elijahharmon@gmail.com>
2018-08-01 14:47:57 -0400

    Added all the requirements for mission 1

--- python/Missions/mission_1/starter_code.py
+++ python/Missions/mission_1/starter_code.py
@@ -4,10 +4,12 @@ Author: ENTER_NAME
 Description: ENTER_DESC
 """
 
+# Write your code in this function
 def main():
     """
     Write your code inside the main function
     """
 
+# Don't worry if you don't know what this does
 if __name__ == '__main__':
     main()
